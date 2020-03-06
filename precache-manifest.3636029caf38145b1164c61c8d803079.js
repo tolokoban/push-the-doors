ec3326d5222278023c508330ac815860c66845f7
Tolokoban <contact@tolokoban.org>
2020-03-06 16:45:12 +0100

    Updates

---
+++ precache-manifest.3636029caf38145b1164c61c8d803079.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b435da07d8801f2349d621e70569c38e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b88f733553df3007cb3f",
+    "url": "./static/css/main.e1d30df3.chunk.css"
+  },
+  {
+    "revision": "192e9f8afbccd7dcacdf",
+    "url": "./static/js/2.f279d401.chunk.js"
+  },
+  {
+    "revision": "e468b20662ce7acec401182a9064b329",
+    "url": "./static/js/2.f279d401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b88f733553df3007cb3f",
+    "url": "./static/js/main.facf9985.chunk.js"
+  },
+  {
+    "revision": "b88639a65afb015e2ade",
+    "url": "./static/js/runtime-main.df03395a.js"
+  },
+  {
+    "revision": "d636a8893ef8144f41e3c0e5a8a50041",
+    "url": "./static/media/atlas-level.d636a889.png"
+  }
+]);
